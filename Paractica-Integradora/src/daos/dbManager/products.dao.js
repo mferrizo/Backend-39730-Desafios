48bfe148cc5c17cda604feb111747fb4531b1952
Matias Ferrizo <matias.ferrizo@gmail.com>
2023-04-15 15:56:02 -0300

    Practica de integracion

---
+++ Paractica-Integradora/src/daos/dbManager/products.dao.js
@@ -0,0 +1,26 @@
+import {productModel} from "../../models/product.model.js";
+
+class ProductDao {
+    async getAll() {
+        return await productModel.find()
+    }
+    async getById(id) {
+        return await productModel.findById(id)
+    }
+
+    async create(data) {
+        return await productModel.create(data)
+    }
+
+    async update(id, data) {
+        return await productModel.findByIdAndUpdate(id, data, {
+            new: true
+        })
+    }
+
+    async delete(id) {
+        return await productModel.findByIdAndDelete(id)
+    }
+}
+
+export default new ProductDao()
